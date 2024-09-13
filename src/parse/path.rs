50a1af291f497575aab03b05f786e5ae67bd84a5
hacrvlq <hacrvlq@proton.me>
2024-09-13 17:33:05 +0200

    feat(parse): Support nested placeholders
    Note that correct error handling is missing.

--- src/parse/path.rs
+++ src/parse/path.rs
@@ -1,165 +1,25 @@
-use std::{env, ops::RangeInclusive, path::PathBuf};
+use std::{env, iter, ops::Range, path::PathBuf};
 
 use crate::paths;
 
-#[derive(Debug, derive_more::From, derive_more::Display)]
-pub enum Error {
-	#[display("illformed")]
-	Illformed,
-	#[display("expands to invalid UTF-8")]
-	InvalidUtf8,
-	#[display("{}", Self::display_env_var(name, err))]
-	EnvVar { name: String, err: env::VarError },
-	#[display("unknown variable")]
-	UnkownVariable,
-	#[display("could not find this include file")]
-	IncludeFileNotFound,
-	#[display("found multiple matching files: {files:?}")]
-	MultipleMatchingIncludeFiles { files: Vec<PathBuf> },
-	#[display("this path has to be absolute")]
-	RelativePath,
-
-	#[allow(clippy::enum_variant_names)]
-	#[from]
-	PathsError(paths::Error),
-}
-impl Error {
-	fn display_env_var(name: &str, err: &env::VarError) -> String {
-		match err {
-			env::VarError::NotPresent => format!("environment variable `{name}` not found"),
-			env::VarError::NotUnicode(_) => {
-				format!("environment variable `{name}` was not valid unicode")
-			}
-		}
-	}
-}
-impl std::error::Error for Error {}
-type ModResult<T> = Result<T, Error>;
+type ModResult<T> = Result<T, std::convert::Infallible>;
 
 pub fn canonicalize_path(path: impl Into<String>) -> ModResult<PathBuf> {
 	let path = PathBuf::from(substitute_placeholder(path, false)?);
 	if path.is_relative() {
-		return Err(Error::RelativePath);
+		panic!();
 	}
 	Ok(path)
 }
-
-//TODO: nested expressions
-pub fn substitute_placeholder(str: impl Into<String>, allow_file_var: bool) -> ModResult<String> {
-	let mut str = str.into();
-
-	#[derive(Debug)]
-	struct Placeholder {
-		range: RangeInclusive<usize>,
-		square_bracket_range: bool,
-	}
-	impl Placeholder {
-		fn shift(self, amount: usize) -> Self {
-			Self {
-				range: *self.range.start() + amount..=*self.range.end() + amount,
-				..self
-			}
-		}
-	}
-	fn find_next_placeholder(str: &str) -> ModResult<Option<Placeholder>> {
-		let square_start_seq_idx = str.find("$[");
-		let round_start_seq_idx = str.find("$(");
-		let search_square_placeholder = match (square_start_seq_idx, round_start_seq_idx) {
-			(None, None) => return Ok(None),
-			(Some(_), None) => true,
-			(None, Some(_)) => false,
-			(Some(square_idx), Some(round_idx)) => square_idx < round_idx,
-		};
-
-		let start_seq_idx = if search_square_placeholder {
-			square_start_seq_idx.unwrap()
-		} else {
-			round_start_seq_idx.unwrap()
-		};
-		let end_seq_idx = str[start_seq_idx..]
-			.find(if search_square_placeholder { ']' } else { ')' })
-			.ok_or(Error::Illformed)?
-			+ start_seq_idx;
-
-		Ok(Some(Placeholder {
-			range: start_seq_idx..=end_seq_idx,
-			square_bracket_range: search_square_placeholder,
-		}))
-	}
-
-	// str[str_pointer..] is the still unfinished substr
-	let mut str_pointer = 0;
-	loop {
-		let Some(next_placeholder) = find_next_placeholder(&str[str_pointer..])? else {
-			break;
-		};
-		let next_placeholder = next_placeholder.shift(str_pointer);
-
-		let expr = &str[next_placeholder.range.start() + 2..=next_placeholder.range.end() - 1];
-		// NOTE: can be None if the expression must be resolved at a later point,
-		//       e.g. $(FILE)
-		let resolved_expr = if next_placeholder.square_bracket_range {
-			Some(resolve_envvar_expr(expr)?)
-		} else {
-			resolve_variable_expr(expr, allow_file_var)?
-		};
-
-		if let Some(resolved_expr) = &resolved_expr {
-			str.replace_range(next_placeholder.range, resolved_expr);
-			str_pointer += resolved_expr.len();
-		} else {
-			str_pointer = next_placeholder.range.end() + 1;
-		}
-	}
-
-	if str.contains('~') {
-		Ok(str.replace(
-			'~',
-			paths::get_home_dir()?.to_str().ok_or(Error::InvalidUtf8)?,
-		))
-	} else {
-		Ok(str)
-	}
-}
-fn resolve_envvar_expr(expr: &str) -> ModResult<String> {
-	let parts = expr.split(':').collect::<Vec<_>>();
-	if parts.is_empty() || parts.len() > 2 {
-		return Err(Error::Illformed);
-	}
-	let env_var_name = parts[0];
-	let env_var_alt = parts.get(1).map(ToString::to_string);
-
-	match env::var(env_var_name) {
-		Ok(value) => Ok(value),
-		Err(env::VarError::NotPresent) if env_var_alt.is_some() => Ok(env_var_alt.unwrap()),
-		Err(err) => Err(Error::EnvVar {
-			name: env_var_name.to_string(),
-			err,
-		}),
-	}
-}
-fn resolve_variable_expr(expr: &str, allow_file_var: bool) -> ModResult<Option<String>> {
-	let path = match expr {
-		"CONFIG" => paths::get_xdg_config_dir()?,
-		"CACHE" => paths::get_xdg_cache_dir()?,
-		"DATA" => paths::get_xdg_data_dir()?,
-		"STATE" => paths::get_xdg_state_dir()?,
-		"FILE" if allow_file_var => return Ok(None),
-		_ => return Err(Error::UnkownVariable),
-	};
-
-	Ok(Some(path.to_str().ok_or(Error::InvalidUtf8)?.to_string()))
-}
-
 pub fn canonicalize_include_path(path: impl Into<String>) -> ModResult<PathBuf> {
-	let path = PathBuf::from(substitute_placeholder(path.into(), false)?);
+	let path = PathBuf::from(substitute_placeholder(path, false)?);
 
 	if path.is_absolute() {
 		return Ok(path);
 	};
 
 	let mut possible_files = Vec::new();
-	for data_root_dir in paths::get_skeld_data_dirs()? {
+	for data_root_dir in paths::get_skeld_data_dirs().unwrap() {
 		let include_root_dir = data_root_dir.join("include");
 		let possible_file_path = include_root_dir.join(&path);
 		if possible_file_path.exists() {
@@ -168,13 +28,168 @@ pub fn canonicalize_include_path(path: impl Into<String>) -> ModResult<PathBuf> 
 	}
 
 	if possible_files.is_empty() {
-		Err(Error::IncludeFileNotFound)
+		panic!();
 	} else if possible_files.len() >= 2 {
-		Err(Error::MultipleMatchingIncludeFiles {
-			files: possible_files,
-		})
+		panic!();
 	} else {
 		assert!(possible_files.len() == 1);
 		Ok(possible_files.into_iter().next().unwrap())
 	}
+}
+
+// resolves all placeholders except $(FILE),
+// allow_file_var determines whether the $(FILE) placeholder is allowed
+pub fn substitute_placeholder(str: impl Into<String>, allow_file_var: bool) -> ModResult<String> {
+	let str = str.into();
+
+	let home_dir = paths::get_home_dir().unwrap();
+	let home_dir = home_dir.to_str().unwrap();
+	let resolve_placeholder = |placeholder| match placeholder {
+		Placeholder::Tilde { idx: pos } => (pos..pos + 1, home_dir.to_string()),
+		Placeholder::BracketPair {
+			ty: BracketType::Square,
+			span,
+			inner_span,
+		} => (span, resolve_envvar_expr(&str[inner_span], allow_file_var)),
+		Placeholder::BracketPair {
+			ty: BracketType::Round,
+			span,
+			inner_span,
+		} => {
+			let resolved_expr = resolve_variable_expr(&str[inner_span], allow_file_var)
+				// preserve variables that need to be resolved later
+				.unwrap_or_else(|| str[span.clone()].to_string());
+			(span, resolved_expr)
+		}
+	};
+
+	let replacements = find_toplevel_placeholders(&str)
+		.into_iter()
+		.map(resolve_placeholder);
+	let substituted_str = replace_multiple_ranges(&str, replacements);
+	Ok(substituted_str)
+}
+fn resolve_envvar_expr(expr: &str, allow_file_var: bool) -> String {
+	let parts = expr.split(':').collect::<Vec<_>>();
+	assert!(!parts.is_empty());
+	if parts.len() > 2 {
+		panic!();
+	}
+	let env_var_name = parts[0];
+	assert!(find_next_placeholder_poi(env_var_name).is_none());
+	let env_var_alt = parts.get(1);
+
+	match env::var(env_var_name) {
+		Ok(value) => value,
+		Err(env::VarError::NotPresent) if env_var_alt.is_some() => {
+			let env_var_alt = *env_var_alt.unwrap();
+			substitute_placeholder(env_var_alt, allow_file_var).unwrap()
+		}
+		Err(_) => panic!(),
+	}
+}
+// NOTE: returns None if the variable needs to be resolved
+//       at a later stage (e.g. $(FILE))
+fn resolve_variable_expr(expr: &str, allow_file_var: bool) -> Option<String> {
+	assert!(find_next_placeholder_poi(expr).is_none());
+
+	let path = match expr {
+		"CONFIG" => paths::get_xdg_config_dir().unwrap(),
+		"CACHE" => paths::get_xdg_cache_dir().unwrap(),
+		"DATA" => paths::get_xdg_data_dir().unwrap(),
+		"STATE" => paths::get_xdg_state_dir().unwrap(),
+		"FILE" if allow_file_var => return None,
+		_ => panic!(),
+	};
+
+	Some(path.to_str().unwrap().to_string())
+}
+
+#[derive(Clone)]
+enum Placeholder {
+	Tilde {
+		idx: usize,
+	},
+	BracketPair {
+		ty: BracketType,
+		span: Range<usize>,
+		inner_span: Range<usize>,
+	},
+}
+#[derive(Copy, Clone, PartialEq, Eq)]
+enum BracketType {
+	Square,
+	Round,
+}
+fn find_toplevel_placeholders(str: &str) -> Vec<Placeholder> {
+	let mut placeholders = Vec::new();
+
+	let mut bracket_stack = Vec::new();
+	let mut str_pointer = 0;
+	while let Some((rel_span, ty)) = find_next_placeholder_poi(&str[str_pointer..]) {
+		let idx = str_pointer + rel_span.start;
+		str_pointer += rel_span.end;
+		match ty {
+			PlaceholderPoI::Tilde if bracket_stack.is_empty() => {
+				placeholders.push(Placeholder::Tilde { idx })
+			}
+			PlaceholderPoI::Tilde => (),
+			PlaceholderPoI::Bracket { ty, opening: true } => bracket_stack.push((idx, ty)),
+			PlaceholderPoI::Bracket { ty, opening: false } => {
+				let matching_opening_bracket = bracket_stack.pop().unwrap();
+				assert!(matching_opening_bracket.1 == ty);
+				if bracket_stack.is_empty() {
+					placeholders.push(Placeholder::BracketPair {
+						ty,
+						span: matching_opening_bracket.0..idx + 1,
+						inner_span: matching_opening_bracket.0 + 2..idx,
+					});
+				}
+			}
+		}
+	}
+	assert!(bracket_stack.is_empty());
+
+	placeholders
+}
+enum PlaceholderPoI {
+	Bracket { ty: BracketType, opening: bool },
+	Tilde,
+}
+fn find_next_placeholder_poi(str: &str) -> Option<(Range<usize>, PlaceholderPoI)> {
+	#[rustfmt::skip]
+	let poi_types = [
+		("$[", PlaceholderPoI::Bracket {ty: BracketType::Square, opening: true }),
+		("$(", PlaceholderPoI::Bracket {ty: BracketType::Round, opening: true }),
+		("]", PlaceholderPoI::Bracket {ty: BracketType::Square, opening: false }),
+		(")", PlaceholderPoI::Bracket {ty: BracketType::Round, opening: false }),
+		("~", PlaceholderPoI::Tilde),
+	];
+	poi_types
+		.into_iter()
+		.filter_map(|(poi_str, ty)| {
+			let poi_start_pos = str.find(poi_str)?;
+			Some((poi_start_pos..poi_start_pos + poi_str.len(), ty))
+		})
+		.min_by_key(|(span, _)| span.start)
+}
+
+fn replace_multiple_ranges(
+	str: &str,
+	replacements: impl IntoIterator<Item = (Range<usize>, String)>,
+) -> String {
+	let replacements = replacements
+		.into_iter()
+		.chain(iter::once((str.len()..str.len(), String::new())));
+
+	let mut result_string = String::new();
+	let mut str_idx = 0;
+	for replacement in replacements {
+		assert!(str_idx <= replacement.0.start);
+		result_string.push_str(&str[str_idx..replacement.0.start]);
+		result_string.push_str(&replacement.1);
+		str_idx = replacement.0.end;
+	}
+
+	result_string
 }
